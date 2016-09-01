86f07a303d9ba59ce463d655ff30d035197dfea4
Hoang Vuong <1264012@gmail.com>
2016-09-01 15:52:39 +0700

    course in student

--- public/controllers/semesterController.js
+++ public/controllers/semesterController.js
@@ -629,7 +629,11 @@ angular.module('smsApp-semestersList', ['ngRoute', 'datatables', 'ngResource', '
                             $scope.scheduleDates[i].time = new Date ($rootScope.course.scheduleDate[i].time);
                         }  
                         
-                        $scope.dateOff = $rootScope.course.dateOff; 
+                        $scope.dateOff = $rootScope.course.dateOff;
+                        for (var i = 0; i < $rootScope.course.dateOff.length; i++) {
+                            $scope.dateOff[i].dateOffStart = new Date ($rootScope.course.dateOff[i].dateOffStart);
+                            $scope.dateOff[i].dateOffEnd = new Date ($rootScope.course.dateOff[i].dateOffEnd);
+                        } 
                         console.log($scope.scheduleDates);                   
 
                         $scope.courseSubmit = function() {
@@ -645,7 +649,7 @@ angular.module('smsApp-semestersList', ['ngRoute', 'datatables', 'ngResource', '
                                 .then(
                                     function(response) {
                                         notifications.showSuccess({
-                                            message: 'Add Course successfully.'
+                                            message: 'Edit Course successfully.'
                                         });
                                         $uibModalInstance.close(true);
                                     },
@@ -685,7 +689,7 @@ angular.module('smsApp-semestersList', ['ngRoute', 'datatables', 'ngResource', '
         $location, $uibModal, Semester, notifications, Course, Professor, Grade) {
 
         // set grade for student base course ID 
-        $scope.addGradeForStudent = function(courseID) {
+        $scope.addGradeForStudent = function(courseID, courseName) {
             Grade.get(courseID).success(function(res) {
                 $rootScope.grade = res.grade;
                 console.log(courseID);
@@ -694,6 +698,7 @@ angular.module('smsApp-semestersList', ['ngRoute', 'datatables', 'ngResource', '
                     animation: true,
                     templateUrl: 'templates/professors/setGrade.html',
                     controller: function($scope, $uibModalInstance, Professor, Grade) {
+                        $scope.gradeTitle = courseName
                         $scope.studentID = $rootScope.grade.studentID;
                         $scope.point = [];
                         $scope.addGrade = function() {
